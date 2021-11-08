c2b1ebb493842d2cefdc633496d8ea4025544914
BornofWind22 <93020731+BornofWind22@users.noreply.github.com>
2021-11-08 14:49:33 -0800

    Create guessing_game.py
    The following code was written by myself and is designed to meet the exceeds expectations requirement for project 1 on the tech degree python track. The game will ask the player if they would like to play, give them a set of parameters in which to enter, and once finished ask if they would like to play again or tell them to have a great day. 
    I look forward to any and all feedback I can get on this code and wish to utilize said feedback to build a better foundation in my Python knowledge. 
    Thank you for stopping by!

---
+++ guessing_game.py
@@ -0,0 +1,32 @@
+print("=========================")
+print("Number Guessing Game")
+print("=========================")
+import random
+def start_game():
+    player_input= input("Would you like to play a number guessing game? [Yes/No] ")
+    random_number = random.randint(1,10)
+    attempts = 0
+    while player_input.lower() == "yes":
+        try:
+            question = int(input("Please pick a number between 1 and 10: "))
+            attempts += 1
+            if question < 1:
+                print("That is outside of the range 1 and 10")
+            elif question > 10:
+                print("That is outside of the range 1 and 10")
+            elif question > random_number:
+                print("It's lower")
+            elif question < random_number:
+                print("It's higher")
+            else:
+                print("That is correct! You picked the right number in {} tries.".format(attempts))
+                print("We hope you enjoyed playing the Number Guessing Game")
+                new_game = input("Would you like to play again? [Yes/No] ")
+                if new_game.lower() == "yes":
+                    start_game()
+                break
+        except ValueError:
+            print("Oh no, invalid entry. That is not a number between 1 and 10")
+            continue
+    print("Have a great day")    
+start_game()
